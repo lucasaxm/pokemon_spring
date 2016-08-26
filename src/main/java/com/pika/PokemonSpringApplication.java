6a9634540cb4ec729add4c485e8d58c4e2e4e358
Lucas Morais <lucas.axm@gmail.com>
2016-08-26 15:26:13 -0300

    Project created on IntelliJ

---
+++ src/main/java/com/pika/PokemonSpringApplication.java
@@ -0,0 +1,12 @@
+package com.pika;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokemonSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokemonSpringApplication.class, args);
+	}
+}
